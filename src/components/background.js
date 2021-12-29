430efd456bf8cc92403e3277fee28b67624a274b
unknown <my0990@naver.com>
2021-12-29 15:51:21 +0900

    third commit

--- src/components/background.js
+++ src/components/background.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import backgroundImg from '../img/background.png'
+import cannon from '../img/cannon.png'
 
 
 const Container = styled.div`
     background-image: url(${backgroundImg});
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100vw;
+    height: 0;
+    padding-bottom: 56.26%;
+    background-color: hotpink;
+    position: relative;
+    overflow; hidden;
+    @media screen and (min-width: 768px){
+        background-color: gray;
+    }
+`
+
+const Cannon = styled.div`
+    background-image: url(${cannon});
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
-    width: 100%;
-    height: 100vh;
-    
-    
+    width: 100vw;
+    height: 17.626vw;
+    position: absolute;
+    bottom: 0;
 `
 function Background(){
     return(
         <Container>
-           
+           <Cannon />
+           {window.innerWidth}
         </ Container>
     )
 }
